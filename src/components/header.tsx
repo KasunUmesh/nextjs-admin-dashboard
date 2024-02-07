5911b9f6727f91e0e719d9c81aa144ea65f9abb4
Kasun Umesh <kasunumesh38@gmail.com>
2024-02-08 01:57:57 +0530

    header created

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,30 @@
+import { TiThMenu } from "react-icons/ti";
+import { FaRegBell, FaEnvelope } from "react-icons/fa";
+
 export default function Header() {
     return (
-        <div>Header</div>
+        <header className="fixed w-full z-0 px-4 shadow-lg pl-[20rem]">
+            <div className="flex items-center justify-between h-16">
+                <button className="hover:bg-[#3a3f38] ml-3 rounded-md h-[30px] w-[30px] shadow-md shadow-black/10 transition duration-300 ease-in-out flex items-center justify-center">
+                    <TiThMenu></TiThMenu>
+                </button>
+
+                <div className="flex items-center gap-[15px] relative ">
+                    <div className="flex items-center gap-[25px] border-r-[1px] pr-[25px]">
+                        <FaRegBell/>
+                        <FaEnvelope/>
+                    </div>
+
+                    <div className="flex items-center gap-[15px] relative">
+                        <p>Kasun Umesh</p>
+                        <div className="h-10 w-10 rounded-full bg-[#3a3f48] flex items-center justify-center text-center cursor-pointer">
+                            <span className="font-semibold text-sm">KU</span>
+                        </div>
+                    </div>
+  
+                </div>
+             
+            </div>
+        </header>
     )
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image"
+
 export default function Sidebar() {
     return (
         <aside className="fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-lg shadow-gray-900/20 transition duration-300 ease-in-out w-[20rem]">
-            <div>
-                <image></image>
+            <div className="flex relative items-center py-5 px-3.5">
+                <Image alt="" src='/logo.png' width={35} height={35} className="w-12 mx-3.5 min-h-fit"></Image>
+                <h3 className="pl-2 font-bold text-2xl text-[#e6e9ee] min-w-max">
+                    SB ADMIN
+                </h3>
             </div>
+
+            <nav>
+                
+            </nav>
         </aside>
     )
 }
