14dd4936a052f4ae9c9514273b8def48b87cfe70
Kasun Umesh <kasunumesh38@gmail.com>
2024-02-08 00:36:42 +0530

    sidebar, header and pagewrapper class created

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+    return (
+        <aside className="fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-lg shadow-gray-900/20 transition duration-300 ease-in-out w-[20rem]">
+            <div>
+                <image></image>
+            </div>
+        </aside>
+    )
+}
